3d0ffe010c74588301e146ce15dd74b62ba84221
njosepha2 <njosepha2@yahoo.com>
2021-06-06 11:36:36 +0100

    Initial Commit: data summary and preliminary analysis added.

--- main.py
+++ main.py
@@ -2,5 +2,10 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+# Importing the data set from our location using pandas.
 df=pd.read_csv("/home/markv/Downloads/gg/covid_19_india.csv")
 print(df.head(10))
+print(df.shape)
+# Performing preliminary analysis.
+print(df.info())
+print(df.describe())
