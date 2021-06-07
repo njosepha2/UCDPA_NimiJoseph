f212b3facb2627726ce3dbcfdb976669292f484e
njosepha2 <njosepha2@yahoo.com>
2021-06-07 10:35:19 +0100

    Indepth analysis of state added

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+
 # A function called barPlot has been defined here.
 def barPlot(covid19_df_latest,columnName,colour):
         plt.figure(figsize=(18, 18), dpi=90)
@@ -7,6 +9,13 @@ def barPlot(covid19_df_latest,columnName,colour):
                 align='center', color=colour)
         plt.ylabel("Number of "+columnName+" Cases", size=20)
         plt.title("States with maximum "+columnName+" cases", size=20)
+        plt.show()
+
+# A function called fatalityCureRatioPlot has been defined here.
+def fatalityCureRatioPlot(covid19_df_latest,columnName,colour):
+        plt.figure(figsize=(20,10))
+        plt.title(columnName)
+        sns.pointplot(data=covid19_df_latest,x='State',y=columnName,color=colour)
         plt.show()
 
 # Importing the data set from our location using pandas.
@@ -17,6 +26,7 @@ print(df.shape)
 # Performing preliminary analysis.
 print(df.info())
 print(df.describe())
+
 # Performing data cleaning.
 print(df.isnull().sum())
 
@@ -31,15 +41,30 @@ dataFrame.Confirmed = dataFrame.Confirmed.fillna('0')
 del dataFrame["ConfirmedIndianNational"]
 del dataFrame["ConfirmedForeignNational"]
 print(dataFrame.isnull().sum())
+
 # Renaming the column to better suite our operation and finding the unique value in the new State column.
 dataFrame.rename(columns = {'State/UnionTerritory':'State'},inplace=True)
 print(dataFrame['State'].unique())
+
 # Plotting a bar graph considering Total Confirmed, Cured, Active and Death cases in the country.
 
 # Plotting a bar graph to see the top five states confirmed covid 19 cases in the country.
 covid19_df_latest = dataFrame[dataFrame['Date']=="2021-05-08"]
 print(covid19_df_latest.head())
-barPlot(covid19_df_latest.sort_values(by=['Confirmed'], ascending = False),"Confirmed","#9e9518")
+covid19_df_latest_confirmed=covid19_df_latest
+barPlot(covid19_df_latest_confirmed.sort_values(by=['Confirmed'], ascending = False),"Confirmed","#9e9518")
 
 # Plotting a bar graph to see the top five states cured covid 19 cases in the country.
-barPlot(covid19_df_latest.sort_values(by=['Cured'], ascending = False),"Confirmed","green")
+covid19_df_latest_cured=covid19_df_latest
+barPlot(covid19_df_latest_cured.sort_values(by=['Cured'], ascending = False),"Confirmed","green")
+
+# Indepth analysis of 4 major states
+dataFrame=dataFrame.loc[(dataFrame['State'] == 'Kerala') | (dataFrame['State'] == 'Maharashtra')| (dataFrame['State'] == 'Karnataka')|(dataFrame['State'] == 'Tamil Nadu')]
+
+# Here I have plotted a line graph to show both the fatality rate and cure rate.
+covid_df1 = dataFrame.tail(2000)
+covid_df2 = covid_df1.sort_values(by='Confirmed', ascending=False).head(2000)
+covid_df2['Fatality-Ratio'] = covid_df2['Deaths']/covid_df2['Confirmed']
+fatalityCureRatioPlot(covid_df2,"Fatality-Ratio","red")
+covid_df2['Cure-Ratio'] = covid_df2['Cured']/covid_df2['Confirmed']
+fatalityCureRatioPlot(covid_df2,"Cure-Ratio","blue")
